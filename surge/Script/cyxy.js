d93c124399d7c2b0142777400f77fd8fdffe7a9e
Eric <Alex0510@users.noreply.github.com>
2020-04-11 20:24:31 +0800

    Delete cyxy.js

--- surge/Script/cyxy.js
+++
@@ -1,9 +0,0 @@
-/*
-彩云小译解锁VIP
-https://api.interpreter.caiyunai.com/v1/user/
-*/
-
-let obj = JSON.parse($response.body);
-obj.user.biz["is_xy_vip"] = true,
-obj.user.biz["xy_vip_expire"] = 4078109706
-$done({body: JSON.stringify(obj)});
