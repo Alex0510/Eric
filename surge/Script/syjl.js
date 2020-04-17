13d6ecee9f9cf66f03c5385dcd368481428823ed
Eric <Alex0510@users.noreply.github.com>
2020-04-17 19:09:56 +0800

    水印精灵

--- surge/Script/syjl.js
+++ surge/Script/syjl.js
@@ -3,15 +3,20 @@
 https://api1.dobenge.cn/api/user/getuserinfo
 */
 
-let obj = JSON.parse($response.body);
 
-  obj. data= {
-    "user_id": 叫爸爸,
+var obj = JSON.parse($response.body);
+
+obj = {
+  "status": 200,
+  "msg": "获取成功",
+  "data": {
     "nick_name": "我是你爸爸",
-    "head_img":"https://img2.woyaogexing.com/2017/12/28/2faa32ef81d9d6dc!400x400_big.jpg",
+    "gender": 0,
+    "mobile": "",
     "is_vip": 1,
-    "vip_end_time": "2099-12-15 12:15:12"
+    "vip_end_time": "2099-12-15 12:15:12",
+    "user_id": 888888,
+    "head_img": "https://img2.woyaogexing.com/2017/12/28/2faa32ef81d9d6dc!400x400_big.jpg"
   }
-
-
+}
 $done({body: JSON.stringify(obj)});
