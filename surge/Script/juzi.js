ce582fbba4769b807cf8a3d15f041cc509257666
Eric <Alex0510@users.noreply.github.com>
2020-04-29 11:16:44 +0800

    句读句子控

---
+++ surge/Script/juzi.js
@@ -0,0 +1,14 @@
+const path1 = "/api/v9/users/profile";
+const path2 = "/mobi/me/";
+
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+obj.is_member = true,
+obj.member_expired_at = 4569874528
+}
+if ($request.url.indexOf(path2) != -1){
+obj.data.memberType = "1",
+obj.data.removeSplashAd = true
+}
+$done({body: JSON.stringify(obj)});
