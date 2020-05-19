49a475caec57d986c25f0215a355e41e86301698
Eric <Alex0510@users.noreply.github.com>
2020-05-19 15:53:06 +0800

    Update photosync.js

--- surge/Script/photosync.js
+++ surge/Script/photosync.js
@@ -4,7 +4,7 @@ photosync.js = type=http-response,pattern=https://api.revenuecat.com/v1/receipts
 hostname:api.revenuecat.com
 */
 
-
-var obj = JSON.parse($response.body);
-obj = {"request_date":"2020-05-18T13:55:53Z","request_date_ms":1589810153144,"subscriber":{"entitlements":{"premium":{"expires_date":"2029-06-01T13:49:38Z","product_identifier":"com.touchbyte.PhotoSync.PremiumYearly","purchase_date":"2020-05-18T13:49:38Z"}},"first_seen":"2020-05-18T13:46:53Z","last_seen":"2020-05-18T13:46:53Z","management_url":"itms-apps://apps.apple.com/account/subscriptions","non_subscriptions":{},"original_app_user_id":"791CC13A-DC04-47AF-9FC6-FA8D3684110F","original_application_version":"305","original_purchase_date":"2020-05-18T13:16:26Z","other_purchases":{},"subscriptions":{"com.touchbyte.PhotoSync.PremiumYearly":{"billing_issues_detected_at":null,"expires_date":"2029-06-01T13:49:38Z","is_sandbox":false,"original_purchase_date":"2020-05-18T13:49:41Z","period_type":"trial","purchase_date":"2020-05-18T13:49:38Z","store":"app_store","unsubscribe_detected_at":null}}}}
+let obj = JSON.parse($response.body);
+obj.subscriber.entitlements = {"premium":{"expires_date":"2029-06-01T13:49:38Z","product_identifier":"com.touchbyte.PhotoSync.PremiumYearly","purchase_date":"2020-05-18T13:49:38Z"}},
+obj.subscriber.subscriptions = {"com.touchbyte.PhotoSync.PremiumYearly":{"billing_issues_detected_at":null,"expires_date":"2029-06-01T13:49:38Z","is_sandbox":false,"original_purchase_date":"2020-05-18T13:49:41Z","period_type":"trial","purchase_date":"2020-05-18T13:49:38Z","store":"app_store","unsubscribe_detected_at":null}}
 $done({body: JSON.stringify(obj)});
