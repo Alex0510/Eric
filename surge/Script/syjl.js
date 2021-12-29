6dc837215195a71acd34f7d59c867327acb4e57f
Eric <Alex0510@users.noreply.github.com>
2021-12-29 22:56:09 +0800

    Update syjl.js

--- surge/Script/syjl.js
+++ surge/Script/syjl.js
@@ -1,22 +1,24 @@
 /*
 水印精灵VIP
-https://api1.dobenge.cn/api/user/getuserinfo
+http://removewatermark.4fqp.com/auth/index
 */
 
 
 var obj = JSON.parse($response.body);
 
 obj = {
-  "status": 200,
-  "msg": "获取成功",
+ {
+  "msg": "success",
   "data": {
-    "nick_name": "我是你爸爸",
-    "gender": 0,
-    "mobile": "",
+    "nick_name": "sy_1640788023",
+    "phone": "17684726444",
+    "status": 1,
+    "id": 882767,
     "is_vip": 1,
-    "vip_end_time": "2099-12-15 12:15:12",
-    "user_id": 888888,
-    "head_img": "https://img2.woyaogexing.com/2017/12/28/2faa32ef81d9d6dc!400x400_big.jpg"
-  }
+    "end_time": "2099-12-15 12:15:12",
+    "channel": "other"
+  },
+  "code": 200
+}
 }
 $done({body: JSON.stringify(obj)});
