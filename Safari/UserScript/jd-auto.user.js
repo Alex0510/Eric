e62897889e71a29cc563293ed2b33070d7deb634
Eric <Alex0510@users.noreply.github.com>
2022-06-30 06:47:36 +0800

    Update jd-auto.user.js

--- Safari/UserScript/jd-auto.user.js
+++ Safari/UserScript/jd-auto.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         京东任务自动完成
-// @version      1.0.0
+// @version      1.0.1
 // @description  Run jd_scripts in your browser
 // @author       Eric
 // @grant        GM_getValue
@@ -16,13 +16,13 @@
 // @match        *://*/*
 // ==/UserScript==
 //##简介：
-打开任意页面会触发脚本自动运行并检查是否需要执行相应的任务。
-每个任务运行最大间隔 3 小时。
-##如何食用：
-升级到最新版本油猴，在设置中通过链接导入 script.user.js 。
-修改脚本顶部的 cookies 为自己的 cookie。
-##已知问题：
-只测试了最新版 Chrome。
+//打开任意页面会触发脚本自动运行并检查是否需要执行相应的任务。
+//每个任务运行最大间隔 3 小时。
+//##如何食用：
+//升级到最新版本油猴，在设置中通过链接导入 script.user.js 。
+//修改脚本顶部的 cookies 为自己的 cookie。
+//##已知问题：
+//只测试了最新版 Chrome。
 (function () {
   const cookies = [
     "pt_pin=jd_652d521e59733;pt_key=AAJfoNW3ADD-1NLWTSxKk4dhtnLhGnvHnRZSdDaB5IP5t0eKPROTSahhwwTJXZUksLklyOBCCkQ;",
