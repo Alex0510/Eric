740e799679729e35e2b132e07435fcd885311434
Eric <Alex0510@users.noreply.github.com>
2022-08-31 00:27:09 +0800

    Update SearchArtifact.js

--- surge/Script/SearchArtifact.js
+++ surge/Script/SearchArtifact.js
@@ -2,10 +2,7 @@
 
 let obj = JSON.parse($response.body);
 
-obj.data.vipType = 1,
-obj.data.vipPastDueTime = 2222222222222,
-obj.data.vipLabelLevel = 3,
-obj.data.vitalityVipPastDueTime = 2222222222222,
-    
+obj.data.vipType = 1024,
+
 
 $done({body: JSON.stringify(obj)});
