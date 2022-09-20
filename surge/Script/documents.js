045928970d9ede4f0784f6152e06fc2705278071
Eric <Alex0510@users.noreply.github.com>
2022-09-20 16:57:16 +0800

    Update documents.js

--- surge/Script/documents.js
+++ surge/Script/documents.js
@@ -1,6 +1,6 @@
 /*
 documents
-https://license.pdfexpert.com/api/1.0/documents/subscription/refresh
+https://license.pdfexpert.com/api/2.0/documents/subscription/refresh
 hostname：license.pdfexpert.com
 */
 
@@ -9,13 +9,37 @@ hostname：license.pdfexpert.com
 var obj = JSON.parse($response.body);
 
 obj ={
-"isEligibleForIntroPeriod":true,
-"originalTransactionId":"689300364339354",
-"subscriptionExpirationDate":"12:25 12/12/2029",
-"subscriptionState":"active",
-"subscriptionReceiptId":"152304284583",
-"isDocuments6User":true,
-"subscriptionAutoRenewStatus":"autoRenewOff",
-"isInGracePeriod":true
+  "originalTransactionId": 20001113828089,
+  "inAppStates": [
+    {
+      "originalTransactionId": 20001113828089,
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.month10_allusers",
+      "subscriptionState": "trial",
+      "productName": "subscription",
+      "subscriptionExpirationDate": "08:52 27\/09\/2099",
+      "isEligibleForIntroPeriod": false,
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "type": "subscription",
+      "isInGracePeriod": false,
+      "isInBillingRetryPeriod": false,
+      "entitlements": [
+      ]
+    },
+    {
+      "originalTransactionId": "0000",
+      "entitlements": [
+      ],
+      "type": "custom purchase",
+      "productId": "documents6-user"
+    }
+  ],
+  "chargingPlatform": "iOS AppStore",
+  "receiptStatus": "ok",
+  "bundleId": "com.readdle.ReaddleDocsIPad",
+  "receiptId": 1495937666000,
+  "statisticsInfo": {
+    "events": [
+    ]
+  }
 }
 $done({body: JSON.stringify(obj)});
