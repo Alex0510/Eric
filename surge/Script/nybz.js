ecb6787c1241034dcfb2d911c49853a977fd6d44
Eric <Alex0510@users.noreply.github.com>
2022-10-12 20:21:44 +0800

    Update nybz.js

--- surge/Script/nybz.js
+++ surge/Script/nybz.js
@@ -2,7 +2,7 @@
 奶由壁纸解锁VIP--Eric
 [rewrite_local]
 
-https://nz-api.duitang.com/account/me/ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/dzqjzz.js
+https://nz-api.duitang.com/account/me/ url script-response-body https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Script/nybz.js
 
 [mitm] 
 
