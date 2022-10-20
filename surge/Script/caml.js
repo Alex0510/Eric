c9cb1562ac8d24812b7d7b6f3befc25a82746c48
Eric <Alex0510@users.noreply.github.com>
2022-10-20 11:01:29 +0800

    Update caml.js

--- surge/Script/caml.js
+++ surge/Script/caml.js
@@ -58,7 +58,51 @@ if ($request.url.indexOf(path1) != -1){
 obj["subscription"] = key;
 }
 if ($request.url.indexOf(path2) != -1){
-obj["subscription"] = key;
+obj["subscription"] = {
+  "is_refunded": false,
+  "ios_details": {
+    "id": "160000700663112",
+    "began": "2020-03-13T03:20:31.000Z",
+    "purchaser_platform": "ios",
+    "is_free_trial": true,
+    "is_renewable": true,
+    "is_in_billing_retry_period": false,
+    "will_renew": false,
+    "expires": "2099-03-20T03:20:31.000Z",
+    "is_canceled": true,
+    "payment_processor": "itunes",
+    "free_trial_began": "2020-03-13T03:20:31.000Z",
+    "original_transaction_id": "160000700663112",
+    "plan_duration": "year",
+    "product_id": "com.calm.yearly.usd_60",
+    "free_trial_length_in_days": 7,
+    "free_trial_ended": "2066-03-20T03:20:31.000Z"
+  },
+  "subscription_plan": "com.calm.yearly.usd_60",
+  "has_ever_had_android_subscription": false,
+  "has_ever_done_free_trial": true,
+  "user_id": "6pLV_HjP-",
+  "plan_duration": "year",
+  "free_trial_ended": "2099-03-20T03:20:31.000Z",
+  "purchaser_platform": "ios",
+  "is_in_billing_retry_period": false,
+  "free_trial_began": "2020-03-13T03:20:31.000Z",
+  "has_ever_had_ios_subscription": true,
+  "is_canceled": true,
+  "will_renew": false,
+  "is_lifetime": true,
+  "type": "ios",
+  "free_trial_length_in_days": 7,
+  "is_free": false,
+  "id": "160000700663112",
+  "expires": "2099-03-20T03:20:31.000Z",
+  "valid": false,
+  "payment_processor": "itunes",
+  "began": "2020-03-13T03:20:31.000Z",
+  "in_free_trial_window": false,
+  "is_renewable": true,
+  "conversion_value": 0
+}
 }
 if ($request.url.indexOf(path3) != -1){
 obj = {
