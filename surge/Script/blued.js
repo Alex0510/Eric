6fc4e966461aebb81f5a7a2a64f2ecfbefc042a0
Eric <Alex0510@users.noreply.github.com>
2024-07-08 11:41:00 +0800

    更新 blued.js

--- surge/Script/blued.js
+++ surge/Script/blued.js
@@ -2,6 +2,7 @@ var anye = JSON.parse($response.body);
 const Eric = /users\/shadow/;
 const Eric1 = /users\/.*\/basi/;
 const Eric2 = /users\/.*is_living=.*/;
+const Eric3 = /users\?extra_info=.*/;
 
 if (Eric.test($request.url) && anye.data && anye.data.length > 0) {
     // 地图显示头像和影子功能
@@ -11,6 +12,10 @@ if (Eric.test($request.url) && anye.data && anye.data.length > 0) {
 if (Eric1.test($request.url) && anye.data && anye.data.length > 0) {
     // 聊天界面查看会员隐藏的距离
     anye.data[0].is_hide_distance = 0;
+}
+if (Eric3.test($request.url) && anye.data && anye.data.length > 0) {
+    // 地图找人试用到期
+    anye.data[0].code = 200;
 }
 if (Eric2.test($request.url)) {
     // 查看最后登录时间而不是最后运行时间仅供参考不准
