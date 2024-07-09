89ccd32ea48c47f9b50f3339c025dea9b55ccd03
Eric <Alex0510@users.noreply.github.com>
2024-07-09 15:42:19 +0800

    更新 Blued.js

--- surge/Script/Blued.js
+++ surge/Script/Blued.js
@@ -2,8 +2,12 @@
 let url = $request.url;
 
 // 从 Surge 传递的参数中获取自定义经纬度值
-let customLongitude = $arguments.longitude || "111.619512";
-let customLatitude = $arguments.latitude || "40.815888";
+let customLongitude = $arguments.customLongitude || "111.619512";
+let customLatitude = $arguments.customLatitude || "40.815888";
+
+// 打印日志以调试参数读取
+console.log(`Custom Longitude: ${customLongitude}`);
+console.log(`Custom Latitude: ${customLatitude}`);
 
 // 使用正则表达式同时匹配并替换 longitude 和 latitude 的值
 let rewrittenURL = url.replace(/(latitude|lat|longitude|lot)=\d+\.\d+/g, (match) => {
@@ -15,6 +19,9 @@ let rewrittenURL = url.replace(/(latitude|lat|longitude|lot)=\d+\.\d+/g, (match)
     return match;
 });
 
+// 打印日志以调试 URL 修改
+console.log(`Original URL: ${url}`);
+console.log(`Rewritten URL: ${rewrittenURL}`);
 
 // 直接替换URL中的 sort_by=nearby 为 sort_by=online
 rewrittenURL = rewrittenURL.replace('sort_by=nearby', 'sort_by=online');
