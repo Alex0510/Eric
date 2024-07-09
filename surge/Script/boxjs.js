a181e7eb5062d13b4f824fc693836584aadf1e3a
Eric <Alex0510@users.noreply.github.com>
2024-07-09 20:32:24 +0800

    更新 boxjs.js

---
+++ surge/Script/boxjs.js
@@ -0,0 +1,39 @@
+{
+   "id": "wuhuhuuuu.app.sub",
+   "name": "wuhuhuuuu",
+   "author": "@wuhuhuuuu",
+   "icon": "https://github.com/wuhuhuuuu/study/raw/main/wuhuhuuuu.PNG",
+   "repo": "https://github.com/Yu9191",
+   "apps": [
+      {
+         "id": "ipReplacement",
+         "name": "IP 替换",
+         "keys": [
+            "customLatitude",
+            "customLongitude"
+         ],
+         "settings": [
+            {
+               "id": "customLatitude",
+               "name": "自定义纬度",
+               "val": "31.220570627554054",
+               "type": "text",
+               "desc": "设置自定义纬度值"
+            },
+            {
+               "id": "customLongitude",
+               "name": "自定义经度",
+               "val": "121.48001048292042",
+               "type": "text",
+               "desc": "设置自定义经度值"
+            }
+         ],
+         "author": "@baby",
+         "script": "https://raw.githubusercontent.com/Yu9191/Rewrite/main/buled_ip.js",
+         "repo": "https://Yu9191",
+         "icons": [
+            "https://github.com/wuhuhuuuu/study/raw/main/wuhuhuuuu.PNG"
+         ]
+      }
+   ]
+}
