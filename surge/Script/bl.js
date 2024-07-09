6d19dea017af86fab4f698362166fda09363c695
Eric <Alex0510@users.noreply.github.com>
2024-07-09 19:06:26 +0800

    更新 bl.js

--- surge/Script/bl.js
+++ surge/Script/bl.js
@@ -1,30 +1,12 @@
-// 获取请求的URL
-let url = $request.url;
+// 获取传递的参数
+const customLongitude = $argument.Longitude || '116.40133452594137';
+const customLatitude = $argument.Latitude || '39.89503534593025';
 
-// 从 Surge 传递的参数中获取自定义经纬度值
-let customLongitude = $arguments.customLongitude || "116.40133452594137";
-let customLatitude = $arguments.customLatitude || "39.89503534593025";
-
-// 打印日志以调试参数读取
-console.log(`Custom Longitude: ${customLongitude}`);
-console.log(`Custom Latitude: ${customLatitude}`);
-
-// 使用正则表达式同时匹配并替换 longitude 和 latitude 的值
-let rewrittenURL = url.replace(/(latitude|lat|longitude|lot)=\d+\.\d+/g, (match) => {
-    if (match.startsWith('latitude') || match.startsWith('lat')) {
-        return match.split('=')[0] + '=' + customLatitude;
-    } else if (match.startsWith('longitude') || match.startsWith('lot')) {
-        return match.split('=')[0] + '=' + customLongitude;
-    }
-    return match;
+// 修改 URL 中的参数
+var modifiedUrl = $request.url.replace(/(longitude|lot)=\d+\.\d+|(latitude|lat)=\d+\.\d+/g, function(match, p1, p2) {
+    if (p1) return 'longitude=' + customLongitude;
+    if (p2) return 'latitude=' + customLatitude;
+    return match; 
 });
 
-// 打印日志以调试 URL 修改
-console.log(`Original URL: ${url}`);
-console.log(`Rewritten URL: ${rewrittenURL}`);
-
-// 直接替换URL中的 sort_by=nearby 为 sort_by=online
-rewrittenURL = rewrittenURL.replace('sort_by=nearby', 'sort_by=online');
-
-// 设置重写后的URL
-$done({ url: rewrittenURL });
+$done({url: modifiedUrl});
