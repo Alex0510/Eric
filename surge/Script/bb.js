51fb9cd6be39e85dc1aeaaddd62509c4e4e30ffb
Eric <Alex0510@users.noreply.github.com>
2024-07-11 22:54:31 +0800

    更新 bb.js

--- surge/Script/bb.js
+++ surge/Script/bb.js
@@ -1,51 +1,32 @@
-const PASSWORD = "Eric1069";
+// 模块链接
+const moduleUrl = "https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Module/blued.sgmodule";
 
-const userPassword = $persistentStore.read("userPassword");
+// 预设密码
+const correctPassword = "123456";
 
-if (!userPassword) {
-  $notification.post("请输入密码", "点击此通知输入密码", "surge://prompt?title=输入密码&hint=请输入密码&key=userPassword");
-  $done({ response: { status: 403, body: "Forbidden: No Password Provided" } });
-  return;
-}
+// 弹出密码输入框
+const enteredPassword = prompt("请输入密码以添加模块链接：");
 
-if (userPassword !== PASSWORD) {
-  $notification.post("密码错误", "请检查你的密码", "");
-  $done({ response: { status: 403, body: "Forbidden: Incorrect Password" } });
-  return;
-}
-// 获取响应体
-let body = $response.body;
-let anye = JSON.parse(body);
-
-const Eric = /users\/shadow/;
-const Eric1 = /users\/.*\/basic/;
-const Eric2 = /users\/.*is_living=.*/;
-const Eric3 = /users\?extra_info=.*/;
-
-if (Eric.test($request.url) && anye.data && anye.data.length > 0) {
-    // 地图显示头像和影子功能
-    anye.data[0].is_open_shadow = 1;
-    anye.data[0].has_right = 1;
-}
-if (Eric1.test($request.url) && anye.data && anye.data.length > 0) {
-    // 聊天界面查看会员隐藏的距离
-    anye.data[0].is_hide_distance = 0;
-    anye.data[0].is_hide_last_operate = 0;
-}
-if (Eric3.test($request.url) && anye.data && anye.data.length > 0) {
-    // 地图找人试用到期
-    anye.data[0].code = 200;
-}
-if (Eric2.test($request.url)) {
-   
-    // 查看隐藏头像
-    anye.data.forEach(user => {
-        user.avatar = user.latest_avatar;
-    });
-    // 查看最后登录时间而不是最后运行时间仅供参考不准
-    let modifiedBody = JSON.stringify(anye);
-    modifiedBody = modifiedBody.replace(/"is_hide_last_operate":\d/, '"is_hide_last_operate":0').replace(/"last_login"/, '"last_operate"');
-    $done({ body: modifiedBody });
+// 验证密码
+if (enteredPassword === correctPassword) {
+  // 密码正确，添加模块链接
+  $httpClient.get(moduleUrl, function(error, response, data) {
+    if (error) {
+      $notification.post("模块添加失败", "无法获取模块链接", error);
+    } else {
+      // 解析模块内容并添加到 Surge 配置中
+      const moduleConfig = JSON.parse(data);
+      // 假设你有一个函数可以将模块内容添加到 Surge 配置中
+      addModuleToSurgeConfig(moduleConfig);
+      $notification.post("模块添加成功", "模块已成功添加到 Surge 配置中", "");
+    }
+  });
 } else {
-    $done({ body: JSON.stringify(anye) });
+  // 密码错误，显示错误通知
+  $notification.post("密码错误", "无法添加模块链接", "请检查密码后重试");
+}
+
+// 假设你有一个函数可以将模块内容添加到 Surge 配置中
+function addModuleToSurgeConfig(moduleConfig) {
+  // 这里添加你的逻辑，将模块内容添加到 Surge 配置中
 }
