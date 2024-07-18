aef62445b15d79ff1837956262fff5a178c8e1d2
Eric <Alex0510@users.noreply.github.com>
2024-07-18 22:17:18 +0800

    更新 finka.js

--- surge/Script/finka.js
+++ surge/Script/finka.js
@@ -1,3 +1,4 @@
+//
 (async () => {
     try {
         // Base64 编码函数
@@ -11,7 +12,7 @@
         }
 
         // Base64编码
-        const encryptedPassword = 'RXJpYzEwNjk=';
+        const encryptedPassword = 'RXJpYzEwNjk='; 
 
         // 从 BoxJS 获取密码配置
         const boxjsPassword = $persistentStore.read('EricPassword');
@@ -107,14 +108,16 @@
             }
         }
 
-        // 修改请求头中的 X-App-Location
+       // 修改请求头中的 X-App-Location
         let headers = $request.headers || {};
         headers["X-App-Location"] = `${latitude},${longitude}`;
         headers["x-app-location"] = `${latitude},${longitude}`;
         console.log('Set X-App-Location:', headers["X-App-Location"]);
-console.log('Set x-app-location:', headers["x-app-location"]);
+        console.log('Set x-app-location:', headers["x-app-location"]);
+
         // 修改请求体中的参数
         let body = $request.body || "";
+        console.log('Original Body:', body);
 
         // 使用正则表达式匹配并替换参数
         body = body.replace(/(count=)[0-9]+/, `$19999`);
