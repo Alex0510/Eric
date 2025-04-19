445ba9f5b23edfed986dc173f81acdb4ec61081e
Eric <Alex0510@users.noreply.github.com>
2025-04-19 22:34:22 +0800

    更新 axworkschedule.js

--- surge/Script/axworkschedule.js
+++ surge/Script/axworkschedule.js
@@ -1,2 +1,9 @@
+/*
+排班日历解锁vip等级
+[mitm]
+hostname = schedule-api.julanling.com
+[script]
+排班日历 = type=http-response,pattern=(http|s)://schedule-api.julanling.com/api/,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Alex0510/Eric/refs/heads/master/surge/Script/axworkschedule.js
+*/
 body = $response.body.replace(/\"grade\":\d/, "\"grade\":3")
 $done({body});
